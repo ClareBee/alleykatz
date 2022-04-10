3a2415b384ed01adf96797d8378ef1fc6c0fda9a
Clare <clareblackburne@pensionbee.com>
2022-04-10 18:09:59 +0100

    Typescript correction

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -5,7 +5,7 @@ export const calculateVote = (votes: Vote[]): number => {
   return upVotes.length - (votes.length - upVotes.length);
 };
 
-export const getUserVote = (votes: Vote[], userId?: string) => {
+export const getUserVote = (votes: Vote[], userId?: string | null) => {
   if (!votes || votes.length === 0 || !userId) return;
   const vote = votes.find((vote: Vote) => vote.sub_id === userId);
   return { vote: vote?.value, id: vote?.id }
